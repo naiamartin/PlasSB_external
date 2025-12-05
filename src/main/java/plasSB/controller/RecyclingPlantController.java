a5b328b491571432a62dc3939b5740972d151c34
Maddi Esparta <maddi.esparta@opendeusto.es>
2025-12-05 14:00:44 +0100

    capacity va

--- src/main/java/plasSB/controller/RecyclingPlantController.java
+++ src/main/java/plasSB/controller/RecyclingPlantController.java
@@ -22,7 +22,7 @@ public class RecyclingPlantController {
     
     // GET capacidad actual
     @GetMapping("/capacity/current")
-    public float getCurrentCapacity(@RequestParam String plant_name) {
+    public float getCurrentCapacity(@RequestParam("plant_name") String plant_name) {
         return recyclingPlantService.getCapacity(plant_name);
     }
     
@@ -48,14 +48,12 @@ public class RecyclingPlantController {
             @RequestParam String plant_name,
             @RequestBody NotificationDTO notification) {
         try {
-        	float amount= notification.getTons();
-        	recyclingPlantService.updateCapacity(plant_name, amount);
-//            recyclingPlantService.receiveNotification(
-//                plant_name,
-//                notification.getDumpsters(),
-//                notification.getPackages(),
-//                notification.getTons()
-//            );
+            recyclingPlantService.receiveNotification(
+                plant_name,
+                notification.getDumpsters(),
+                notification.getPackages(),
+                notification.getTons()
+            );
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
